35c6595b42efe600169f6246e2164b82a003a785
Emiliano Lafuente <emilianolafuente@hotmail.com>
2024-01-30 19:54:25 -0300

    Notas y desafio clase 02

--- Entrega Clase 02/desafio.js
+++ Entrega Clase 02/desafio.js
@@ -1,28 +1,57 @@
-let personas = [{
+let products = []
+let id = 0
 
-    id: 1,
-    name: "Diego",
-    mayorEdad: true,
-},
-{
-    id: 2,
-    name: "Laura",
-    mayorEdad: true,
-},
-{
-    id: 3,
-    name: "Ruben",
-    mayorEdad: true,
+//metodo getProducts desafio
+const getProducts = () => (products)
+
+//metodo addProducts desafio
+function addProducts (title, description, price, thumbnail, code, stock) {
+
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+        console.log('All fields are required' + " " + title,description,code)
+
+      }
+
+    if(products.find(product => product.code === code)){
+        console.log("Not found" + " " + title,description,code)
+    }else{
+        const product = {
+            id: addId(),
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            code: code,
+            stock: stock
+        }
+        products.push(product)
+    }
+
+
 }
-]
-//Object.entries
-let entries = personas.map(persona => Object.entries(persona))
-//console.log(entries);
 
-//Object.keys
-let keys = personas.map(persona => Object.keys(persona))
-console.log(keys);
+function addId () {
+    if(products.length === 0){
+        id = 1
+    }else{
+        id = products[products.length-1].id + 1
+    }
 
-//Object.values
-let values = personas.map(persona => Object.values(persona))
-console.log(values);
+    return id
+}
+
+//metodo getProductById desafio
+function getProductById(id) {
+    return products.find(product => product.id === id);
+  }
+  
+//Testing del codigo
+const producto1 = new addProducts("pc","pc",100,"asd1.jpg",1,5)
+const producto2 = new addProducts("pc2","pc2",100,"asd2.jpg",2,5)
+const producto3 = new addProducts("pc3","pc3",100,"asd3.jpg",1,5)
+const producto4 = new addProducts("pc4","pc4",100,"asd4.jpg",4,5)
+const producto5 = new addProducts("pc4","pc4",100,"",4,5)
+  
+console.log(products);
+
+console.log(getProductById(2));
