c3e14b72965cf9e98cc48608b1c5e9e214e65dc6
EmilianoLafuente <emilianolafuente1094@gmail.com>
2024-02-16 09:30:32 -0300

    Desafio Clase 06

--- Entrega Clase 06/desafioEntregable/app.js
+++ Entrega Clase 06/desafioEntregable/app.js
@@ -11,45 +11,41 @@ app.get("/products", (req, res) => {
     try {
         const limit = parseInt(req.query.limit, 10);
 
-        // Llama al método y maneja la Promesa correctamente
         productsManager.getProducts().then((products) => {
-            console.log("🚀 ~ app.get ~ products:", products);
-
-            const limitedProducts = limit ? products.slice(0, limit) : products;
+            const limitedProducts = limit ? products.slice(0, limit) : products; //http://localhost:8080/products?limit=3
             res.json({ products: limitedProducts });
+ 
         }).catch((error) => {
             console.error(error.message);
             res.status(500).json({ error: 'Error al procesar la solicitud' });
+ 
         });
-
-    } catch (error) {
+       } catch (error) {
         console.error(error.message);
         res.status(500).json({ error: 'Error al procesar la solicitud' });
+         }
+});
+
+app.get("/products/:pid", (req, res) => {
+    try {
+        const id = parseInt(req.params.pid) //http://localhost:8080/products/5
+        console.log(id);
+        productsManager.getProductById(id).then((prodcutById) => {
+            res.json(prodcutById);
+
+        }).catch((error) => {
+            console.error(error.message);
+            res.status(500).json({ error: 'Error al procesar la solicitud' });
+ 
+        });
+
+        } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: 'Error al procesar la solicitud' });
+
     }
 });
 
-
-
-//  app.get("/products", (req, res) => {
-//      try {
-//         const limit = parseInt(req.query.limit, 10);
-
-        
-//         // const products =  productsManager.getProducts()
-//         // console.log("🚀 ~ app.get ~ products:", products)
-
-        
-//         // res.send()
-//         //const limitedProducts = limit ? products.slice(0, limit) : products
-
-//         //res.json({ products: limitedProducts })
-        
-//     } catch (error) {
-//         console.error(error.message);
-//         res.status(500).json({ error: 'Error al procesar la solicitud' })
-//     }
-
-// });
 
 const server = app.listen(PORT, () => {
   console.log(`Open server on port ${PORT}`);
@@ -58,27 +54,6 @@ const server = app.listen(PORT, () => {
 server.on("error", (error) => console.log("Server error:", error));
 
 
-// app.get("/productos", (req, res) => {
-//   productos
-//     .getAll()
-//     .then((data) => res.send(data))
-//     .catch((error) => {
-//       console.log(error.message);
-//       res.send({ error: error.message });
-//     });
-// });
 
-// app.get("/productoRandom", (req, res) => {
-//   productos
-//     .getAll()
-//     .then((data) => {
-//       const random = Math.floor(Math.random() * data.length);
-//       res.send(data[random]);
-//     })
-//     .catch((error) => {
-//       console.log(error.message);
-//       res.send({ error: error.message });
-//     });
-// });
 
 
