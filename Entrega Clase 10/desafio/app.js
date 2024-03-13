a066c15536eb72953d40a8d8df224ca1a65b77b8
Emiliano Lafuente <emilianolafuente@hotmail.com>
2024-03-12 22:36:57 -0300

    Entregable

--- Entrega Clase 10/desafio/app.js
+++ Entrega Clase 10/desafio/app.js
@@ -1,20 +1,20 @@
 import express from "express"
-import cartsRouter from "./src/routes/cartsRouter.js"
-import productsRouter from "./src/routes/productsRouter.js"
 import __dirname from "../src/utils.js";
 import handlebars from "express-handlebars";
 import { Server } from "socket.io";
 import viewRouter from './src/routes/viewRouter.js'
+import cartsRouter from "./src/routes/cartsRouter.js"
+import productsRouter from "./src/routes/productsRouter.js"
 
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
 //middlewares
-app.set('views', __dirname +'/views')
+app.set('views',__dirname +'/views')
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname +'/public'))
 app.engine('handlebars', handlebars.engine())
 
 //app.use(viewRouter)
@@ -23,11 +23,31 @@ app.engine('handlebars', handlebars.engine())
 //routes
 app.use("/api/carts", cartsRouter)
 app.use("/api/products", productsRouter)
-app.use("/home", viewRouter)
+app.use(viewRouter)
 
 //servidorON
-const server = app.listen(PORT, () => {console.log(`Open server on port ${PORT}`);
+const serverHttp = app.listen(PORT, () => {console.log(`Open server on port ${PORT}`);
 });
-const io = new Server(server) //socket.io
 
-server.on("error", (error) => console.log("Server error:", error));
+serverHttp.on("error", (error) => console.log("Server error:", error));
+
+const io = new Server(serverHttp) //socket.io
+
+const logs = []
+
+io.on('connection', socket =>{
+
+    console.log("Connected!")
+
+    socket.on("message", (data)=> {
+    console.log("🚀 ~ socket.on ~ data:", data)
+        console.log(__dirname);
+    logs.push({socketid: socket.id, message: data})
+        
+        io.emit('log',{logs})
+        console.log("🚀 ~ logs:", logs)
+        
+    })
+
+})
+
--- Entrega Clase 10/desafio/src/routes/viewRouter.js
+++ Entrega Clase 10/desafio/src/routes/viewRouter.js
@@ -1,10 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-const router = Router()
+const router = Router();
 
-
-router.get('/', (req, res)  => {
-    res.render('home')
+router.get('/',(req,res)=>{
+    res.render('home');
 })
 
-export default router
+export default router;
