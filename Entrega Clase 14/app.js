bc6a373bb2e8d11f60e80d016d72bc4c32292376
EmilianoLafuente <emilianolafuente1094@gmail.com>
2024-03-20 16:39:49 -0300

    Actualizacion GIT

---
+++ Entrega Clase 14/app.js
@@ -0,0 +1,30 @@
+import express from "express"
+import mongoose from "mongoose"
+import studentsRouter from "./routes/studentsRouter.js"
+
+const app = express()
+const PORT = 8080
+const server = app.listen(PORT, ()=> console.log("Server running in", PORT))
+
+//const DB_URL = "mongodb+srv://admin:<admin>@cluster0.xtezbaa.mongodb.net/"
+//const connection = mongoose.connect(DB_URL)
+
+
+const connectMongoDB = async ()=>{
+    const stringConnection = 'mongodb+srv://admin:<admin>@cluster0.u9vxzcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' //todo pasar a .env
+
+
+    try {
+        await mongoose.connect(stringConnection);
+        console.log("Conectado con exito a MongoDB usando Moongose.");
+    } catch (error) {
+        console.error("No se pudo conectar a la BD usando Moongose: " + error);
+        process.exit();
+    }
+};
+connectMongoDB();
+
+
+app.use(express.json())
+
+app.use(studentsRouter)
